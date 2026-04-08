bd50aa0541c1067ef644c9b8649df489630404f4
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2026-04-08 12:39:53 +0200

    Refactor: recursive solution in FortyTen and SendMoreMoney classes

--- entregas/FigueroaMarco/sendMoreMoney.java
+++ entregas/FigueroaMarco/sendMoreMoney.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class SendMoreMoneySolver {
+class SendMoreMoneySolver {
 
     public static void main(String[] args) {
         String[] words = {"SEND", "MORE"};
@@ -10,11 +10,7 @@ public class SendMoreMoneySolver {
         boolean[] usedDigits = new boolean[10];
 
         if (solve(letters, 0, mapping, usedDigits, words, result)) {
-            int i = 0;
-            while (i < letters.length) {
-                System.out.println(letters[i] + ": " + mapping[letters[i]]);
-                i++;
-            }
+            printMapping(letters, mapping, 0);
         }
     }
 
@@ -52,46 +48,70 @@ public class SendMoreMoneySolver {
     }
 
     private static boolean isValid(String[] words, String result, int[] mapping) {
-        long sumWords = 0;
-        int i = 0;
-        while (i < words.length) {
-            sumWords += toNumber(words[i], mapping);
-            i++;
-        }
+        long sumWords = sumWords(words, mapping, 0);
         return sumWords == toNumber(result, mapping);
     }
 
     private static long toNumber(String word, int[] mapping) {
-        long residual = 0;
-        int i = 0;
-        while (i < word.length()) {
-            residual = residual * 10 + mapping[word.charAt(i)];
-            i++;
-        }
-        return res;
+        return toNumber(word, mapping, 0, 0);
     }
 
     private static boolean isLeading(char character, String[] words, String result) {
         if (result.charAt(0) == character) return true;
-        int i = 0;
-        while (i < words.length) {
-            if (words[i].charAt(0) == character) return true;
-            i++;
-        }
-        return false;
+        return isLeadingInWords(character, words, 0);
     }
 
     private static char[] getUniqueLetters(String[] words, String result) {
         String all = words[0] + words[1] + result;
-        String unique = "";
-        int i = 0;
-        while (i < all.length()) {
-            char character = all.charAt(i);
-            if (unique.indexOf(character) == -1) {
-                unique += character;
-            }
-            i++;
-        }
+        String unique = collectUnique(all, 0, "");
         return unique.toCharArray();
+    }
+
+    private static void printMapping(char[] letters, int[] mapping, int index) {
+        if (index == letters.length) {
+            return;
+        }
+
+        System.out.println(letters[index] + ": " + mapping[letters[index]]);
+        printMapping(letters, mapping, index + 1);
+    }
+
+    private static long sumWords(String[] words, int[] mapping, int index) {
+        if (index == words.length) {
+            return 0;
+        }
+
+        return toNumber(words[index], mapping) + sumWords(words, mapping, index + 1);
+    }
+
+    private static long toNumber(String word, int[] mapping, int index, long current) {
+        if (index == word.length()) {
+            return current;
+        }
+
+        long next = current * 10 + mapping[word.charAt(index)];
+        return toNumber(word, mapping, index + 1, next);
+    }
+
+    private static boolean isLeadingInWords(char character, String[] words, int index) {
+        if (index == words.length) {
+            return false;
+        }
+
+        if (words[index].charAt(0) == character) {
+            return true;
+        }
+
+        return isLeadingInWords(character, words, index + 1);
+    }
+
+    private static String collectUnique(String all, int index, String unique) {
+        if (index == all.length()) {
+            return unique;
+        }
+
+        char character = all.charAt(index);
+        String nextUnique = unique.indexOf(character) == -1 ? unique + character : unique;
+        return collectUnique(all, index + 1, nextUnique);
     }
 }
